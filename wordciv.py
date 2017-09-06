f3e4cc200de321cd861c4965986cd001f1d2aa2f
Ernie Ripley <efripley@gmail.com>
2017-09-06 13:53:55 -0600

    initial game screens up and running

--- wordciv.py
+++ wordciv.py
@@ -1,25 +1,97 @@
 #! /usr/bin/python3
 
-state = 0
-done = False
+class Game:
+  state = 0
+  done = False
+  name = False
+game = Game();
 
-def showWelcomeScreen():
+
+#Game Screens
+def welcomeScreen():
+  global game
   print('Welcome To Word Civ')
   print('-------------------')
   print('')
-  print('a| new game')
-  print('b| load game')
-  print('c| how to')
-  print('d| quit')
+  print('n| new game')
+  print('l| load game')
+  print('h| how to')
+  print('q| quit')
+  print('')
 
-def welcomeScreenInput():
-  global done
   command = input('\nwhat would you like to do? ')
-  if command == 'd':
-    done = True
+  if command == 'a':
+    game.state = 1
+  elif command == 'b':
+    game.state = 2
+  elif command == 'c':
+    game.state = 3
+  elif command == 'd':
+    game.done = True
     print('quiting...')
 
-while not done:
-  if state == 0:
-    showWelcomeScreen()
-    welcomeScreenInput()
+def newScreen():
+  global game
+  print('New Game')
+  print('--------')
+  print('')
+  print('type \'exit\' to go back')
+
+  command = input('what would you like to name your game? ')
+  if command == 'exit':
+    game.state = 0
+  else:
+    game.name = command
+    game.state = 4
+
+def loadScreen():
+  global game
+  print('Load Game')
+  print('---------')
+  print('')
+  print('type \'delete <game name>\' to delte game')
+  print('type \'exit\' to go back')
+
+  command = input('what would you like to do? ')
+  if command == 'exit':
+    game.state = 0
+
+def HowToScreen():
+  global game
+  print('How To')
+  print('------')
+  print('')
+  print('u| scroll up')
+  print('d| scroll down')
+  print('q| go back')
+
+  command = input('what would you like to do? ')
+  if command == 'u':
+    print('scrolling up')
+  elif command == 'd':
+    print('scrolling down')
+  elif command == 'q':
+    game.state = 0
+
+def gameStartScreen():
+  global game
+  print(game.name)
+  for ch in game.name:
+    print('-', end = '')
+  print('')
+
+  command = input('command: ')
+  if command == 'exit':
+    game.state = 0
+
+while not game.done:
+  if game.state == 0:
+    welcomeScreen()
+  elif game.state == 1:
+    newScreen()
+  elif game.state == 2:
+    loadScreen()
+  elif game.state == 3:
+    howToScreen()
+  elif game.state == 4:
+    gameStartScreen()
